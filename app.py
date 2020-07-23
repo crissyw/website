54db2338cbb80240f6a8ecc729a51024ddbc834e
crissyw <widjaja.crystal@gmail.com>
2020-07-22 18:51:54 -0700

    add reading list

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
 
@@ -9,17 +8,6 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	content = db.Column(db.String(200), nullable=False)
-	date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-	def __repr__(self):
-		return '<Task %r>' % self.id
-
 @app.route('/')
 def index():
 	return render_template('home.html')
@@ -27,6 +15,44 @@ def index():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/reading_list')
+def readingList():
+	management = {
+		'Radical Candor — Kim Scott': {'url': 'https://amzn.to/3eObUr7', 'file_path':'static/images/reading-list/radicalcandor.jpg'},
+		'The Hard Thing About Hard Things — Ben Horowitz': {'url': 'https://amzn.to/32KaBHu', 'file_path':'static/images/reading-list/hardthingabouthardthings.jpg'},
+		'Crucial Conversations': {'url': 'https://amzn.to/39ifoBf', 'file_path':'static/images/reading-list/crucialconversations.jpg'}
+	}
+
+	enduring_company = {
+		'High Growth Handbook — Elad Gil': {'url': 'https://amzn.to/39qDzNN', 'file_path':'static/images/reading-list/highgrowthhandbook.jpg'},
+		'Measure What Matters — John Doerr': {'url': 'https://amzn.to/30QUeGD', 'file_path':'static/images/reading-list/measurewhatmatters.jpg'},
+		'The Advantage: Why Organizational Health Trumps Everything Else In Business — Patrick Lencioni': {'url': 'https://amzn.to/30BcXWv', 'file_path':'static/images/reading-list/theadvantage.jpg'},
+		'High Output Management — Andy Grove': {'url': 'https://amzn.to/2Bqtkwu', 'file_path':'static/images/reading-list/highoutputmanagement.jpg'},
+		'Good to Great: Why Some Companies Make the Leap and Others Don\'t — Jim Collins': {'url': 'https://amzn.to/3ho6wwH', 'file_path':'static/images/reading-list/goodtogreat.jpg'},
+		'Bad Blood: Secrets and Lies in a Silicon Valley Startup — John Carreyrou': {'url': 'https://amzn.to/3hugE7g', 'file_path':'static/images/reading-list/badblood.jpg'}
+	}
+
+	user_empathy = {
+		'The Creative Curve — Allen Gannett': {'url': 'https://amzn.to/2EairzS', 'file_path':'static/images/reading-list/thecreativecurve.jpg'},
+		'The Everything Store: Jeff Bezos and the Age of Amazon — Brad Stone': {'url': 'https://amzn.to/2CVoDen', 'file_path':'static/images/reading-list/theeverythingstore.jpg'},
+		'Made to Stick: Why Some Ideas Survive and Others Die — Chip Heath & Dan Heath': {'url': 'https://amzn.to/2CCUwZp', 'file_path':'static/images/reading-list/madetostick.jpg'}
+	}
+
+	data_literate = {
+		'How Not to Be Wrong: The Power of Mathematical Thinking — Jordan Ellenberg': {'url': 'https://amzn.to/32LPJzE', 'file_path':'static/images/reading-list/hownottobewrong.jpg'},
+		'Algorithms to Live By: The Computer Science of Human Decisions — Brian Christian & Tom Griffiths': {'url': 'https://amzn.to/2ZOmVob', 'file_path':'static/images/reading-list/algostoliveby.jpg'},
+		'Introduction to Probability and Statistics — MIT': {'url': 'https://ocw.mit.edu/courses/mathematics/18-05-introduction-to-probability-and-statistics-spring-2014/class-slides/', 'file_path':'static/images/reading-list/mit.png'},
+		'The Data Warehouse Toolkit: The Definitive Guide to Dimensional Modeling — Ralph Kimball': {'url': 'https://amzn.to/32LZbmC', 'file_path':'static/images/reading-list/datawarehouse.jpeg'}
+	}
+
+	code_literate = {
+		'Bitcoin and Cryptocurrency Technologies (stop at W5) — Princeton University': {'url': 'https://www.coursera.org/learn/cryptocurrency/home/welcome', 'file_path':'static/images/reading-list/coursera-social-logo.png'},
+		'Dank Learning: Generating Memes Using Deep Neural Networks': {'url': 'https://arxiv.org/pdf/1806.04510.pdf', 'file_path':'https://github.com/alpv95/MemeProject/blob/master/Picture1.png?raw=true'},
+		'Introduction to Probability and Statistics — MIT': {'url': 'https://ocw.mit.edu/courses/mathematics/18-05-introduction-to-probability-and-statistics-spring-2014/class-slides/', 'file_path':'static/images/reading-list/mit.png'}
+	}
+
+	return render_template('reading_list.html',management=management,user_empathy=user_empathy,enduring_company=enduring_company,data_literate=data_literate,code_literate=code_literate)
 
 @app.route('/work')
 def work():
@@ -60,49 +86,6 @@ def work():
 @app.route('/actualtest')
 def actualtest():
 	return render_template('index.html')
-
-@app.route('/test')
-def test():
-	if request.method == 'POST':
-		#logic for task
-		task_content = request.form['content']
-		new_task = Todo(content=task_content)
-
-		try:
-			db.session.add(new_task)
-			db.session.commit()
-			return redirect('/')
-		except:
-			return 'There was an issue adding your task.'
-
-	else:
-		tasks = Todo.query.order_by(Todo.date_created).all()
-		return render_template('index.html', tasks=tasks)
-
-@app.route('/delete/<int:id>')
-def delete(id):
-	task_to_delete = Todo.query.get_or_404(id)
-
-	try:
-		db.session.delete(task_to_delete)
-		db.session.commit()
-		return redirect('/')
-	except:
-		return 'There was a problem deleting your task'
-	
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-	task = Todo.query.get_or_404(id)
-
-	if request.method == 'POST':
-		task.content = request.form['content']
-		try:
-			db.session.commit()
-			return redirect('/')
-		except:
-			return 'There was an issue updating your task.'
-	else:
-		return render_template('update.html',task=task)
 
 def get_static_file(path):
     site_root = os.path.realpath(os.path.dirname(__file__))
